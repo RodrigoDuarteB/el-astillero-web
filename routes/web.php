84b98db9098c69a2a434f889c4fbc44fc69cc444
RodrigoDuarteB <rodrijedbu@gmail.com>
2021-04-24 21:36:10 -0400

    something

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard',
-function () {
+Route::middleware(['auth:sanctum', 'verified'])
+->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
 Route::get('test', function () {
     echo "prueba";
 });
+
